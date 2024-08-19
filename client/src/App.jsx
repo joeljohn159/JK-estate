52f19c1abb235b30191ec5a19fac0fdf6ad051a4
unknown <joe2@gmail.com>
2024-08-19 22:58:49 +0530

    Added pages and Header Component updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+// import { createBrowserRouter, RouterProvider, BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Header from './components/Header';
 
 const App = () => {
+
+  // const router = createBrowserRouter([
+  //   { path:"/", element: <Home/>},
+  //   { path:'/profile', element: <Profile/>},
+  //   { path:'/sign-up', element: <SignUp/>},
+  //   { path:'/sign-in', element: <SignIn/>},
+  //   { path:'/about', element: <About/>},
+  
+  // ])
   return (
-    <h2 className='text-red-500'>Hellos</h2>
+    <>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+        <Route path='/sign-up' element={<SignUp/>}></Route>
+        <Route path='/sign-in' element={<SignIn/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    {/* <RouterProvider router={router}></RouterProvider> */}
+    </>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='bg-slate-200 shadow-sm'>
+        <div className='flex justify-between max-w-6xl items-center mx-auto p-3' >
+            <Link to= "/">
+            <h1 className='font-medium sm:font-bold text-md sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500'>JK</span>
+                <span className='text-slate-900'>Estate</span>
+            </h1>
+            </Link>
+            {/* <h1 className='font-medium sm:font-bold text-md sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500'>JK</span>
+                <span className='text-slate-900'>Estate</span>
+            </h1> */}
+            <form className='bg-slate-100 p-1 sm:p-2 rounded flex items-center'>
+                <input type="text" placeholder='Search' className='focus:outline-none bg-transparent w-30 sm:w-64 pr-1' />
+                <FaSearch className='text-slate-600 cursor-pointer  ' />
+            </form>
+
+            <ul className='flex gap-4 cursor-pointer'>
+                <Link to='/'><li className='hidden sm:inline hover:underline text-slate-800'>Home</li></Link>
+                <Link to='/about'><li className='hidden sm:inline hover:underline text-slate-800'>About</li></Link>
+                <Link to='/sign-in'><li className=' hover:underline text-slate-800'>SignIn</li></Link>
+            </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
